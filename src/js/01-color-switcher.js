fb13a25da564b120b150fdc321213cfe6d321c85
Ekaterina Silchenko <maksil.17@gmail.com>
2022-10-07 21:11:29 +0300

    add tsak 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+import '../css/common.css';
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
+
+btnStart.addEventListener('click', onClickbtnStart);
+btnStop.addEventListener('click', onClickbtnStop);
+
+function onClickbtnStart(event) {
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    bodyEl.style.backgroundColor = color;
+    btnStart.setAttribute('disabled', 'disabled');
+    btnStop.removeAttribute('disabled');
+  }, 1000);
+}
+
+function onClickbtnStop(event) {
+  clearInterval(timerId);
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', 'disabled');
+}
