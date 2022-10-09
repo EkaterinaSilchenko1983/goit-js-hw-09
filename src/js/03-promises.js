60e58f2bb2f4a138541623d27bb8a1af42ac8d54
Ekaterina Silchenko <maksil.17@gmail.com>
2022-10-09 11:26:22 +0300

    Update promise-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,30 +1,40 @@
 const formEl = document.querySelector('.form');
-const btn = document.querySelector('button');
+const btnEl = document.querySelector('button');
 const delayEl = document.querySelector('input[name="delay"]');
 const stepEl = document.querySelector('input[name="step"]');
 const amountEl = document.querySelector('input[name="amount"]');
-console.log(delayEl);
-const delay = Number(delayEl.value);
-console.log(delayEl.value);
 
-// function createPromise(position, delay) {
-//   return new Promise((resolve, reject) => {
-//     const shouldResolve = Math.random() > 0.3;
+let delay = Number(delayEl.value);
+let step = Number(stepEl.value);
+let amount = Number(amountEl.value);
 
-//     setTimeout(() => {
-//       if (shouldResolve) {
-//         resolve({ position, delay }); // Fulfill
-//       } else {
-//         reject({ position, delay }); // Reject
-//       }
-//     }, delay);
-//   });
-// }
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay }); // Fulfill
+      } else {
+        reject({ position, delay }); // Reject
+      }
+    }, delay);
+  });
+}
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(i + 1, delay + step * i);
+  }
+}
