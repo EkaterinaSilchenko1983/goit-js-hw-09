911e649529013568a4e4b3cb36592418a2cdc604
EkaterinaSilchenko1983 <105131083+EkaterinaSilchenko1983@users.noreply.github.com>
2022-10-09 08:26:46 +0000

    Deploying to gh-pages from @ EkaterinaSilchenko1983/goit-js-hw-09@60e58f2bb2f4a138541623d27bb8a1af42ac8d54 🚀

--- 03-promises.23e5821a.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector(".form"),document.querySelector("button");var e=document.querySelector('input[name="delay"]');document.querySelector('input[name="step"]'),document.querySelector('input[name="amount"]');console.log(e);Number(e.value);console.log(e.value)}();
-//# sourceMappingURL=03-promises.23e5821a.js.map
--- 03-promises.6748a898.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form"),document.querySelector("button");const e=document.querySelector('input[name="delay"]');document.querySelector('input[name="step"]'),document.querySelector('input[name="amount"]');console.log(e);Number(e.value);console.log(e.value);
-//# sourceMappingURL=03-promises.6748a898.js.map
